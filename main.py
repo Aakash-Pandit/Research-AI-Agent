3acc03aab3420ba9fdd406666ba5544bcc671a4d
Aakash-Pandit <aakashpandit366@gmail.com>
2026-03-14 16:21:37 +0530

    fix port type cast from str to int

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uvicorn
 
 from application.app import app
 
-port = os.getenv("API_PORT", 8000)
+port = int(os.getenv("API_PORT", "8000"))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
